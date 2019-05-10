e2bf92aa58ebf4ed62c7dc5cc1d1819f09be869b
Marin <cmarin.1788.cml@gmail.com>
2019-05-09 20:41:22 -0500

    first commit pets

--- js/main.js
+++ js/main.js
@@ -1,41 +1,55 @@
-function getData(){
+$("#submit-button").click(()=>{
+    const name = $("#name").val();
+    const breed = $("#breed").val();
+    const description = $("#description").val();
+    
+    const sugerObject = {name, breed, description};
+    console.log(sugerObject);
+    addPet(sugerObject);
+})
+
+const addPet = (sugerObject)=>{
+    $.post ("https://jquerycrud-ed8dc.firebaseio.com/carlos.json",
+    JSON.stringify(sugerObject),
+     function(data) {
+      console.log(data);
+  }, "json");
+   
+
+}
+
+
+function get(){
     $.ajax({
-        url: "https://jquerycrud-ed8dc.firebaseio.com/dogs.json",
+        url: "https://jquerycrud-ed8dc.firebaseio.com/carlos.json",
         type: "GET",
-        success: function(response){
-            console.log(response);
+        success: function (respuesta){
+        printData(respuesta);    
+        
+        
         }
-    });
+        
+    }) 
 }
 
-var postObject = {
-  'name': 'Gogdo',
-  'breed': 'Black Cat'
+
+function printData(dataToPrint){
+    $.each(dataToPrint,(key,value)=>{
+     console.log(`key ${key}, value ${value}, name ${value.name}, breed: ${value.breed}, description: ${value.description}`) 
+         $(".pintar").append(
+             
+             `<div class="col col-sm-6 col-md-4 mb-4">
+							<div class="card">
+								<img src="img/boris.jpg" class="card-img-top" alt="...">
+								<div class="card-body">
+									<h5 class="card-title"><span>${value.name}</span></h5>
+                                    <p>Raza:  <span>${value.breed}</p>
+									<p class="card-text"><span>${value.description}</span></a>
+                                    <a href="#" class="btn btn-primary">¡Adoptar!</a>
+								</div>
+							</div>
+						</div>`
+         )
+    }) 
 }
 
-function postData(){
-    $.post( "https://jquerycrud-ed8dc.firebaseio.com/dogs.json", JSON.stringify(postObject), function( data ) {
-  console.log( data);
-}, "json");
-}
-
-function updateData(){
-    $.ajax({
-       url: 'https://jquerycrud-ed8dc.firebaseio.com/dogs/-LeJgLYcUIR1OwHL49pW.json',
-       type: 'PUT',
-       data: JSON.stringify(postObject),
-       success: function(response) {
-         console.log(response)
-       }
-    });
-}
-
-function deleteData(){
-    $.ajax({
-       url: 'https://jquerycrud-ed8dc.firebaseio.com/dogs/-LeJgLYcUIR1OwHL49pW.json',
-       type: 'DELETE',
-       success: function(response) {
-         console.log(response)
-       }
-    });
-}
